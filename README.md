f21f5edf607ccf91b0f992a8cc17854c1401858b
reggmack <[200~21286023+reggmack@users.noreply.github.com~>
2024-02-22 22:18:03 -0600

    Add new books to array and display on screen

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ JS library project
 
 Pitfalls:
 Trying to code too many steps without committing. Need to work on one small step at a time and commit more often.
+
+Got stuck for 2 days trying to figure out why adding new books displayed the new array on top of the old array instead of just appending it. All I need to do was clear the displaying divs contents before rendering the array again...damn!
--- js/library.js
+++ js/library.js
@@ -1,7 +1,20 @@
-const myLibrary = [];
+let myLibrary = [];
 const addBookButton = document.getElementById("showAddBookDialog");
 const addBookDialog = document.getElementById("addBookDialog");
 const createBookButton = addBookDialog.querySelector("#createBookButton");
+const bookContainer = document.querySelector("#bookContainer");
+const deleteBookButton = document.createElement('button');
+deleteBookButton.textContent = "Delete";
+const div = document.createElement('div');
+
+let inputAuthor;
+let inputTitle;
+let inputPages;
+let inputRead;
+
+// Add default Books to Library
+const addBookToLibrary = (book) => myLibrary.push(book);
+addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", "295", false));
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -13,18 +26,29 @@ function Book(author, title, pages, read) {
 function createBook() {
   const newBook = new Book();
   // Get the input elements
-  const inputAuthor = document.getElementById("bookAuthor");
-  const inputTitle = document.getElementById("bookTitle");
-  const inputPages = document.getElementById("bookPages");
-  const inputRead = document.getElementById("bookRead");
+  inputAuthor = document.getElementById("bookAuthor");
+  inputTitle = document.getElementById("bookTitle");
+  inputPages = document.getElementById("bookPages");
+  inputRead = document.getElementById("bookRead");
   // Get the values of the input elements
   newBook.author = inputAuthor.value;
   newBook.title = inputTitle.value;
   newBook.pages = inputPages.value;
   newBook.read = inputRead.value;
-  // Add new book to myLibrary array
+  // Add new book to Library
   myLibrary.push(newBook);
+  bookContainer.textContent = "";
+  showBooks();
 }
+
+function showBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    const catalogCard = document.createElement("div");
+    catalogCard.textContent = `${myLibrary[i].author} ${myLibrary[i].title} ${myLibrary[i].pages} ${myLibrary[i].read}`;
+    bookContainer.appendChild(catalogCard);
+  }
+}
+showBooks();
 
 // "Add Book Button" button opens the <dialog> modally
 addBookButton.addEventListener("click", () => {
@@ -32,12 +56,13 @@ addBookButton.addEventListener("click", () => {
 });
 
 // "Add Book to Library" button creates book and adds to myLibrary array
-createBookButton.addEventListener("click", () => {
-    createBook();
-  });
-
 // Prevent the "createBookButton" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
 createBookButton.addEventListener("click", (event) => {
+  createBook();
   event.preventDefault(); // We don't want to submit this fake form
   addBookDialog.close(); // Have to send the select box value here.
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputPages.value = "";
+  inputRead.value = "";
 });
