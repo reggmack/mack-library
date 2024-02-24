616b84d946bd07f89b81bc98b0b4dd1f1f95651b
reggmack <[200~21286023+reggmack@users.noreply.github.com~>
2024-02-23 21:29:16 -0600

    Add buttons for read/not read and deleting books

--- js/library.js
+++ js/library.js
@@ -1,27 +1,32 @@
-let myLibrary = [];
 const addBookButton = document.getElementById("showAddBookDialog");
 const addBookDialog = document.getElementById("addBookDialog");
 const createBookButton = addBookDialog.querySelector("#createBookButton");
 const bookContainer = document.querySelector("#bookContainer");
-const deleteBookButton = document.createElement('button');
-deleteBookButton.textContent = "Delete";
-const div = document.createElement('div');
-
 let inputAuthor;
 let inputTitle;
 let inputPages;
 let inputRead;
 
+let myLibrary = [];
+
 // Add default Books to Library
 const addBookToLibrary = (book) => myLibrary.push(book);
+addBookToLibrary(new Book("Book 1", "Author 1", "1", false));
+addBookToLibrary(new Book("Book 2", "Author 2", "2", true));
+addBookToLibrary(new Book("Book 3", "Author 3", "3", false));
+addBookToLibrary(new Book("Book 4", "Author 4", "4", true));
 addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", "295", false));
 
-function Book(author, title, pages, read) {
-  this.author = author;
+function Book(title, author, pages, read) {
   this.title = title;
+  this.author = author;
   this.pages = pages;
   this.read = read;
 }
+
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
 
 function createBook() {
   const newBook = new Book();
@@ -34,7 +39,7 @@ function createBook() {
   newBook.author = inputAuthor.value;
   newBook.title = inputTitle.value;
   newBook.pages = inputPages.value;
-  newBook.read = inputRead.value;
+  newBook.read = inputRead.checked;
   // Add new book to Library
   myLibrary.push(newBook);
   bookContainer.textContent = "";
@@ -42,13 +47,34 @@ function createBook() {
 }
 
 function showBooks() {
+  bookContainer.textContent = "";
   for (let i = 0; i < myLibrary.length; i++) {
     const catalogCard = document.createElement("div");
-    catalogCard.textContent = `${myLibrary[i].author} ${myLibrary[i].title} ${myLibrary[i].pages} ${myLibrary[i].read}`;
+    catalogCard.classList = "catalogCard";
+    catalogCard.innerHTML = `
+    <h2>${myLibrary[i].title}</h2>
+    <div>${myLibrary[i].author}</div>
+    <div>Pages: ${myLibrary[i].pages}</div>
+    <div class="bookButtons">
+    <button class="${myLibrary[i].read ? "isRead" : "notRead"}" onclick="toggleRead(${i}">
+    ${myLibrary[i].read ? "Read" : "Not Read"}</button>
+    <button class="btnDelete" onclick="deleteBook(${i})">Delete</button>
+    </div>
+    `;
     bookContainer.appendChild(catalogCard);
   }
 }
 showBooks();
+
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  showBooks();
+}
+
+function toggleRead(index) {
+  myLibrary[index].toggleRead();
+  showBooks();
+}
 
 // "Add Book Button" button opens the <dialog> modally
 addBookButton.addEventListener("click", () => {
@@ -64,5 +90,5 @@ createBookButton.addEventListener("click", (event) => {
   inputTitle.value = "";
   inputAuthor.value = "";
   inputPages.value = "";
-  inputRead.value = "";
+  inputRead.checked = false;
 });
